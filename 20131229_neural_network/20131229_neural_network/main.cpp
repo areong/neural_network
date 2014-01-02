27dc31c94ac76a347a9cbe2f4e835ec84155f6e5
areong <areong6378@gmail.com>
2014-01-02 17:30:03 +0800

    Add usage of CascadeCorrelationNeuralNetwork to main.

--- 20131229_neural_network/20131229_neural_network/main.cpp
+++ 20131229_neural_network/20131229_neural_network/main.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 using namespace std;
 
-#include "file/FileReader.h"
 #include "data/Data.h"
+#include "file/FileReader.h"
+#include "neuralnetwork/CascadeCorrelationNeuralNetwork.h"
 
 int main(int argc, char *argv[]) {
 
@@ -17,6 +18,13 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 10000; i++) {
         cout << i << '\t' << dataTrain->get_x_i_byIndexAnd_i(0, i) << endl;
     }
+
+    CascadeCorrelationNeuralNetwork *network = new CascadeCorrelationNeuralNetwork();
+    network->initialize(12810, 12);
+    network->setMaxNumHiddenLayers(0);
+    network->setPatience(196);
+    network->setTrainingData(dataTrain);
+    network->run();
 
     system("pause");
     return 0;
